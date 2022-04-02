71e93b2955ca7fea0e8698335ee726c4138ac197
5iveemmy <ainemmanuel87@gmail.com>
2022-04-02 21:36:40 +0100

    create sideBar

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Side = styled.div``;
+const SideWrap = styled.div``;
+
+const SideBar = () => {
+  return (
+    <Side>
+      <SideWrap></SideWrap>
+    </Side>
+  );
+};
+
+export default SideBar;
