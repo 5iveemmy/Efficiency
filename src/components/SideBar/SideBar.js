b08622531ee75ce1c6b7ffbe55fbcb4725e1121d
5iveemmy <ainemmanuel87@gmail.com>
2022-04-03 05:19:16 +0100

    downgrade react version

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,15 +3,30 @@ import styled from "styled-components";
 
 const Side = styled.div`
   height: 100%;
-  width: 300px;
+  width: 262px;
   background-color: red;
 `;
 const SideWrap = styled.div``;
 
+const Meta = styled.div``;
+
+const MetaWrap = styled.div``;
+
+const MetaHead = styled.h2``;
+
+const MetaSmall = styled.p``;
+
 const SideBar = () => {
   return (
     <Side>
-      <SideWrap></SideWrap>
+      <SideWrap>
+        <Meta>
+          <MetaWrap>
+            <MetaHead>Metacare</MetaHead>
+            <MetaSmall>adeyinka@metacare.app</MetaSmall>
+          </MetaWrap>
+        </Meta>
+      </SideWrap>
     </Side>
   );
 };
